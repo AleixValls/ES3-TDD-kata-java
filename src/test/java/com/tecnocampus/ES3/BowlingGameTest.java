d04e5018268652e1ed0f01dc480511fa933af946
AleixValls <103170557+AleixValls@users.noreply.github.com>
2023-04-19 13:16:49 +0200

    Commit18

--- src/test/java/com/tecnocampus/ES3/BowlingGameTest.java
+++ src/test/java/com/tecnocampus/ES3/BowlingGameTest.java
@@ -16,13 +16,14 @@ public class BowlingGameTest {
         rollMany(20,0);
         assertThat(g.score()).isEqualTo(0);
     }
-    private void rollMany(int rolls, int pins) {
-        for (var i = 0; i < rolls; i += 1)
-            g.roll(pins);
-    }
+
     @Test
     public void all_ones() {
         rollMany(20,1);
         assertThat(g.score()).isEqualTo(20);
+    }
+    private void rollMany(int rolls, int pins) {
+        for (var i = 0; i < rolls; i += 1)
+            g.roll(pins);
     }
 }
