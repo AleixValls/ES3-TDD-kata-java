b0cb1addc71bc42d96af1f252209537a4b1de25e
AleixValls <103170557+AleixValls@users.noreply.github.com>
2023-04-19 13:11:27 +0200

    Commit12

--- src/test/java/com/tecnocampus/ES3/BowlingGameTest.java
+++ src/test/java/com/tecnocampus/ES3/BowlingGameTest.java
@@ -17,4 +17,10 @@ public class BowlingGameTest {
             g.roll(0);
         assertThat(g.score()).isEqualTo(0);
     }
+    @Test
+    public void all_ones() {
+        for (var i = 0; i < 20; i += 1)
+            g.roll(1);
+        assertThat(g.score()).isEqualTo(20);
+    }
 }
